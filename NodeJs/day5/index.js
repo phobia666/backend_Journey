0171d32465a85d1f8bf57ea6d8d3f17d23b334bd
Abhyanshu <abhyanshuab05@gmail.com>
2026-07-11 00:16:32 +0530

    added day5

---
+++ NodeJs/day5/index.js
@@ -0,0 +1,18 @@
+// const os = require('os');
+
+
+// // 1. get OS platform and user info
+// console.log("OS Platform: ", os.platform());
+// console.log("USER INFO: ", os.userInfo());
+
+// // 2. get the os cpu core info
+// console.log(os.cpus().length);
+// //and other functions of this os module
+// //use gpt and search
+
+
+
+//now crypto time
+
+const os = require("crypto")
+
