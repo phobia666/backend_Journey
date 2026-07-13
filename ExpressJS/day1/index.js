d597143e91965ef8a2e64ca373129fd904e910aa
Abhyanshu <abhyanshuab05@gmail.com>
2026-07-13 17:22:22 +0530

    all requests done in express

--- ExpressJS/day1/index.js
+++ ExpressJS/day1/index.js
@@ -8,9 +8,9 @@ app.use(express.json());
 
 const PORT = 8080;
 
-//-------------------- 1. GET request (it is used for fetching data from the server)
+//-------------------- 1. GET request (it is used for fetching data from the server)------------------------
 
-//get karte time status 200 hota hai    
+//get karte time status 200 hota hai        
 app.get("/", (req, res)=> {
     res.status(200).send("Hello World!")
 })
@@ -43,13 +43,22 @@ app.get("/api/v1/users", (req, res) => {
 //router params
 //req.params me tum id ke form me data pass karte ho
 app.get("/api/v1/users/:id", (req, res) => {
-    console.log(req.params);
+    // console.log(req.params);
     const {id} = req.params;
     const idNum = parseInt(id);
-    console.log(idNum);
+    // console.log(idNum);
 
     const user = userData.find((user) => user.id === idNum)
+    console.log(user);
+
+    if(user === undefined){
+        res.status(200).send("user not found")
+    }
+
+    
     res.status(200).send(user);
+
+    
 
 })
 
@@ -60,20 +69,22 @@ app.get("/api/v1/users/:id", (req, res) => {
 
 
 
-//------------------------- 2. POST request (used to send data to the server)
+
+//------------------------- 2. POST request (used to send data to the server)----------------------------------
 
 //post karte time status 201 hota hai
 app.post("/api/v1/users", (req, res) => {
-    const {name, username} = req.body
+    const {body} = req;
     //jo aap bhej rhe ho frontend se whi na destructure karoge backend me 
-    console.log(name, username);
+    // console.log( username);
+    console.log(body)
 
-    const newUser = {
+    const newUser = {   
         id : userData.length + 1,
-        name: name,
-        username:username
+        ...body
     } 
     userData.push(newUser);
+
 
 
     res.status(201).send({
@@ -81,6 +92,136 @@ app.post("/api/v1/users", (req, res) => {
         user: newUser 
     });
 })
+
+
+
+
+
+
+//------------------------------------- 3. PUT request (UPDATE ALL FIELDS)-------------------------------------
+app.put("/api/v1/users/:id", (req, res) => {
+
+    const {
+        body,
+        params: { id },     
+    } = req;
+    const parseId = parseInt(id);
+    const userIndex = userData.findIndex((user) => user.id === parseId);
+
+    if(userIndex === -1){
+        res.status(404).send("User not found")
+    }
+
+    userData[userIndex] = {
+        id : parseId,
+        ...body
+        //isko yaa to ese bhi likh skte hai
+    //  name: body.name,
+    //  username: body.username
+    //bachhe thodi ho 
+
+    }
+    //bas yhi chiz h ki put request sare data fields ko change kardeta hai
+    //agar sirf ek ko change karna hai to patch request ka use kro
+    
+    res.status(200).send({
+        message: "User updated",
+        data: userData[userIndex]
+    });
+
+})
+
+
+
+
+
+
+
+//------------------------- 4. PATCH request (UPDATE SPECIFIC FIELD)
+
+app.patch("/api/v1/users/:id", (req, res) => {
+
+    const {
+        body,
+        params: { id },     
+    } = req;
+    const parseId = parseInt(id);
+    const userIndex = userData.findIndex((user) => user.id === parseId);
+
+    if(userIndex === -1){
+        res.status(404).send("User not found")
+    }
+
+    userData[userIndex] = {
+        ...userData[userIndex], ...body
+
+    }
+    //isme sare fields intact rehte hai except for the changed field
+    
+    res.status(200).send({
+        message: "User updated",        
+        data: userData[userIndex]
+    });
+    
+})
+
+
+
+
+
+
+
+//------------------------- 5. DELETE request (it is for deleting data on server)
+
+
+app.delete("/api/v1/users/:id", (req, res) => {
+
+    const {id} = req.params;
+    const parseId = parseInt(id);
+
+    const userIndex = userData.findIndex((user) => user.id === parseId);
+
+    if(userIndex === -1){
+        res.status(404).send("User not found")
+    }
+
+    const user = userData[userIndex];
+
+    userData.splice(userIndex, 1);
+
+    res.status(200).send({
+        message: "User got deleted",
+        data: user
+    })
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
