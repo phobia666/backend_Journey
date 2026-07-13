29384bc45acdb0a1cca349080b3eca09d2a78a5f
Abhyanshu <abhyanshuab05@gmail.com>
2026-07-13 13:49:04 +0530

    learning query params

---
+++ ExpressJS/day1/data/data.js
@@ -0,0 +1,9 @@
+const data = [
+  { username: "coder_ace", age: 21, profession: "Student" },
+  { username: "rahul_dev", age: 25, profession: "Software Engineer" },
+  { username: "priya_ml", age: 23, profession: "Data Scientist" },
+  { username: "arjun_web", age: 27, profession: "Full Stack Developer" },
+  { username: "sneha_ux", age: 24, profession: "UI/UX Designer" }   
+];
+
+export default data;
--- ExpressJS/day1/index.js
+++ ExpressJS/day1/index.js
@@ -1,2 +1,43 @@
-console.log('Hello world');
+import express from "express";
+import userData from "./data/data.js";
+const app = express()
+
+const PORT = 8080;
+
+
+// 1. GET request (it is used for fetching data from the server)
+
+// app.get("/", (req, res)=> {
+//     res.status(200).send("Hello World!")
+// })
+
+
+// /api/v1/users hamara endpoint hai
+// and browser by default get request hi hit karta hai
+app.get("/api/v1/users", (req, res) => {
+    
+    //query params          
+    console.log(req.query());
+
+    res.status(200).send(userData);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, (req, res) => {
+    console.log('server is running at Port', PORT);
+})
 
