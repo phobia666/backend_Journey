4a680c94fc6dfc3c127b6f74722adfb334b983c9
Abhyanshu <abhyanshuab05@gmail.com>
2026-07-13 15:08:27 +0530

    added post request

--- ExpressJS/day1/data/data.js
+++ ExpressJS/day1/data/data.js
@@ -1,9 +1,9 @@
 const data = [
-  { username: "coder_ace", age: 21, profession: "Student" },
-  { username: "rahul_dev", age: 25, profession: "Software Engineer" },
-  { username: "priya_ml", age: 23, profession: "Data Scientist" },
-  { username: "arjun_web", age: 27, profession: "Full Stack Developer" },
-  { username: "sneha_ux", age: 24, profession: "UI/UX Designer" }   
+  { id: 1, username: "coder_ace", age: 21, profession: "Student" },
+  { id: 2, username: "rahul_dev", age: 25, profession: "Software Engineer" },
+  { id: 3, username: "priya_ml", age: 23, profession: "Data Scientist" },
+  { id: 4, username: "arjun_web", age: 27, profession: "Full Stack Developer" },
+  { id: 5, username: "sneha_ux", age: 24, profession: "UI/UX Designer" }   
 ];
 
 export default data;
--- ExpressJS/day1/index.js
+++ ExpressJS/day1/index.js
@@ -1,25 +1,56 @@
 import express from "express";
 import userData from "./data/data.js";
 const app = express()
+app.use(express.json()); 
+// is a middleware joki data ko parse karne ke liye use hota hai
+// when a client sends the POST request in JSON format so it needs to be parsed thru a middleware
+//that's what this function does basically 
 
 const PORT = 8080;
 
+//-------------------- 1. GET request (it is used for fetching data from the server)
 
-// 1. GET request (it is used for fetching data from the server)
-
-// app.get("/", (req, res)=> {
-//     res.status(200).send("Hello World!")
-// })
+//get karte time status 200 hota hai    
+app.get("/", (req, res)=> {
+    res.status(200).send("Hello World!")
+})
 
 
 // /api/v1/users hamara endpoint hai
 // and browser by default get request hi hit karta hai
+
+//query params      
+//req.query me tum key value pair me data pass karte ho
 app.get("/api/v1/users", (req, res) => {
     
-    //query params          
-    console.log(req.query());
+    
+    //question mark ke baad wali chiz url me 
+    // console.log(req.query); 
+    const {username} = req.query;
+
+    if(username){
+        const user = userData.filter((user) =>{
+            return(user.username === username);
+        })
+        res.status(200).send(user);
+    }
 
     res.status(200).send(userData);
+})
+
+
+
+//router params
+//req.params me tum id ke form me data pass karte ho
+app.get("/api/v1/users/:id", (req, res) => {
+    console.log(req.params);
+    const {id} = req.params;
+    const idNum = parseInt(id);
+    console.log(idNum);
+
+    const user = userData.find((user) => user.id === idNum)
+    res.status(200).send(user);
+
 })
 
 
@@ -29,9 +60,27 @@ app.get("/api/v1/users", (req, res) => {
 
 
 
+//------------------------- 2. POST request (used to send data to the server)
+
+//post karte time status 201 hota hai
+app.post("/api/v1/users", (req, res) => {
+    const {name, username} = req.body
+    //jo aap bhej rhe ho frontend se whi na destructure karoge backend me 
+    console.log(name, username);
+
+    const newUser = {
+        id : userData.length + 1,
+        name: name,
+        username:username
+    } 
+    userData.push(newUser);
 
 
-
+    res.status(201).send({
+        message: "user created",
+        user: newUser 
+    });
+})
 
 
 
